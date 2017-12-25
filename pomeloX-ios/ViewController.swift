9d22e8ac81ad3c015ea4e365fa61de2b9e5d71e4
skconan <supakit.kr@gmail.com>
2017-12-25 12:21:57 +0700

    add cancel action

--- pomeloX-ios/ViewController.swift
+++ pomeloX-ios/ViewController.swift
@@ -35,6 +35,9 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         yourNameLabel.text = textField.text
     }
     //MARK: Navigation
+    @IBAction func cancelBtn(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
     // This method lets you configure a view controller before it's presented.
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
